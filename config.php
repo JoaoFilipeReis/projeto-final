ea5aa9ae62dfba4e5cfc6dee999273c194903834
João Filipe Reis <joaofreis1984@gmail.com>
2022-11-03 16:56:01 +0000

    First Commit

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+
+$page_title = 'Projeto Final';
+
---
+++ head.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $page_title ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="./public/style.css">
+</head>
+<body>
+    <div>
+        <nav>
+            <div>
+                <h1>SEBASTIÃO ALVES</h1>
+            </div>
+            <div>
+                <ul>
+                    <li><a href=""></a>HOME</li>
+                    <li><a href=""></a>AUTOR</li>
+                    <li><a href=""></a>LIVROS</li>
+                    <li><a href=""></a>IMPRENSA</li>
+                    <li><a href=""></a>CONTACTOS</li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    
