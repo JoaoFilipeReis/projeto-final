46a90d39bd6327213cb0c845cd27390c71703304
João Filipe Reis <joaofreis1984@gmail.com>
2022-12-11 18:58:25 +0000

    Alteração ao Config para envio do projeto para o FileZilla

--- config.php
+++ config.php
@@ -1,11 +1,13 @@
 <?php
 
 // Define as configurações de acesso à base de dados.
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'codemaster');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_HOST', '38.242.141.223');
+define('DB_NAME', 'lipe');
+define('DB_USER', 'lipe');
+define('DB_PASS', '12345');
 define('DB_PORT', '3306');
+
+define('PAGE_URL', 'http://38.242.141.223/lipe/htdocs');
 
 $dsn = 'mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='. DB_PORT;
 $pdo = new PDO($dsn, DB_USER, DB_PASS);
